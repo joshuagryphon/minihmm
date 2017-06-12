3271485a222cb5fdc72ca5297114996d9d78ddd7
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2017-06-12 17:29:18 -0400

    more test methods

--- minihmm/test/common.py
+++ minihmm/test/common.py
@@ -1,25 +1,38 @@
-from numpy.testing import assert_array_equal
+from numpy.testing import (
+    assert_array_equal,
+    assert_almost_equal,
+)
+
 from nose.tools import (
     assert_equal,
-    assert_almost_equal,
+    assert_true,
+#    assert_almost_equal,
     assert_dict_equal,
     assert_list_equal,
     assert_tuple_equal,
     assert_raises,
     )
 
-
 def check_equal(a, b, msg=None):
     assert_equal(a, b, msg)
+
+def check_array_equal(a, b, **kwargs):
+    assert_array_equal(a, b, **kwargs)
+
+def check_almost_equal(a,b,kwargs={}):
+    assert_almost_equal(a,b, **kwargs)
+
+def check_true(a, kwargs={}):
+    assert_true(a, **kwargs)
+
+def check_not_equal(a, b):
+    assert_raises(AssertionError, check_array_equal, a, b)
 
 def check_list_equal(a, b, msg=None):
     assert_list_equal(a, b, msg)
 
 def check_dict_equal(a, b, msg=None):
     assert_dict_equal(a, b, msg)
-
-def check_array_equal(a, b, **kwargs):
-    assert_array_equal(a, b, **kwargs)
 
 def check_tuple_equal(a, b, msg=None):
     assert_tuple_equal(a, b, msg)
